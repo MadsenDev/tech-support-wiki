02b6c650e5bc4f3fdd2d1321d5deb232f31f7a50
Christoffer Madsen <126353638+MadsenDev@users.noreply.github.com>
2023-09-26 16:34:13 +0200

    Removed connection info.

--- api/db.php
+++ api/db.php
@@ -1,9 +1,9 @@
 <?php
 // api/db.php
 $host = 'localhost';
-$db_name = 'madsyrnh_wiki';
-$username = 'madsyrnh_chris';
-$password = 'data2023';
+$db_name = '';
+$username = '';
+$password = '';
 
 try {
     $db = new PDO("mysql:host={$host};dbname={$db_name};charset=utf8mb4", $username, $password);
